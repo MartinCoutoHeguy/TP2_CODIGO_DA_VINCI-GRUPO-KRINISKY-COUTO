6402d850f6e6c771cf30a1a4947044b725ee695f
MartinCoutoHeguy <120978814+MartinCoutoHeguy@users.noreply.github.com>
2024-06-06 19:09:03 -0300

    Add files via upload

---
+++ desencriptador.py
@@ -0,0 +1,8 @@
+from Funciones import desencriptacion
+
+def main() -> None:
+    return desencriptacion()
+
+if __name__ == '__main__':
+    print(main())
+   
---
+++ encriptador.py
@@ -0,0 +1,7 @@
+from Funciones import encriptacion
+
+def main() -> None:
+    encriptacion()
+
+if __name__ == '__main__':
+    main()
